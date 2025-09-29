1a72cc9d095d5ff7e379d7b35c2484a9b2d852db
EmmaErickson258 <erickson38639@students.apsb.org>
2025-09-29 18:05:08 +0000

    9/29

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,9 +18,50 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    function createSawBlade (x,y) {
+    var hitZoneSize = 25;
+    var damageFromObstacle = 10;
+    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone)
+    var obstacleImage = draw.bitmap("img/sawblade.png");
+     sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -25
+      obstacleImage.y = -25
+  }
+    createSawBlade(1000, 165)
+    createSawBlade(400, 165)
+    createSawBlade(600, 250)
 
-    
+function createEnemy(x, y) {
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.rect(50, 50, "red");
+      redSquare.x = -25;
+      redSquare.y = -25;
+      enemy.addChild(redSquare);
+        enemy.x = x;
+        enemy.y = groundY - y;
+        game.addGameItem(enemy);
+        enemy.velocityX = -3;
+        enemy.rotationalVelocity = -2;
 
+    enemy.onPlayerCollision= function () {
+      game.changeIntegrity(-10);
+    }
+
+    enemy.onProjectileCollision= function() {
+      game.increaseScore(100);
+      enemy.fadeOut();
+    }
+  }
+  createEnemy(400, groundY - 10);
+createEnemy(800, groundY - 100);
+createEnemy(1200, groundY - 50);
+
+function createReward(x, y) {
+ 
+}
     function startLevel() {
       // TODO 13 goes below here
 
--- fsd-projects/runtime/js/view/hud.js
+++ fsd-projects/runtime/js/view/hud.js
@@ -34,14 +34,14 @@
         hud = new createjs.Container();
         
         integrity = new createjs.Container();
-        background = draw.rect(104, 20, '#CCC');
-        draw.rect(102, 18, '#FFF', null, null, 1, 1, background);
+        background = draw.rect(104, 20, '#1b7a26ff');
+        draw.rect(102, 18, '#256e38ff', null, null, 1, 1, background);
         integrity.addChild(background);
         
-        txtScore = draw.textfield('score : 000', "19px Arial", '#666', 'left');
+        txtScore = draw.textfield('score : 000', "19px Arial", '#237358ff', 'left');
         hud.addChild(txtScore);
         
-        integrityMeter = draw.rect(1, 16, '#3333CC');
+        integrityMeter = draw.rect(1, 16, '#267a46ff');
         integrityMeter.scaleX = 100;
         integrity.addChild(integrityMeter);
         hud.addChild(integrity);
